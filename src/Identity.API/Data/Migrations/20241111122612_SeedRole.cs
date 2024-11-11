548ba3cd74a7135d8f2cce7a2f7c517df51b1a8e
mkochan11 <kochanowski.michal.11@gmail.com>
2024-11-11 13:28:03 +0100

    EKF-13 feat: add register endpoint

---
+++ src/Identity.API/Data/Migrations/20241111122612_SeedRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "64c2338e-2666-4572-8d24-b1157abf4876", null, "User", "USER" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "64c2338e-2666-4572-8d24-b1157abf4876");
+        }
+    }
+}
