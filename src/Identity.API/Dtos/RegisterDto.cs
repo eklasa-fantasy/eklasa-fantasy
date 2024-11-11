6e258f5db9782a92ae55bf36cf9c7eb2eb8797a6
mkochan11 <kochanowski.michal.11@gmail.com>
2024-11-11 12:59:38 +0100

    EKF-13 feat: add controller and register dto

---
+++ src/Identity.API/Dtos/RegisterDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.API.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string? EmailAddress {  get; set; }
+        
+        [Required]
+        public string? Password { get; set; }
+    }
+}
