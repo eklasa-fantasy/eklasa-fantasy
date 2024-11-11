efbdfa87584d1d956e9e26bf54b24f678683c5d8
mkochan11 <kochanowski.michal.11@gmail.com>
2024-11-11 12:59:38 +0100

    EKF-13 feat: add controller and register dto

---
+++ src/Identity.API/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Identity.API.Dtos;
+using Identity.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+
+        }
+    }
+}
