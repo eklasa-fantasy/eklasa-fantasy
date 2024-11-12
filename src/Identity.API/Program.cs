4f2586c5871de32b811854da7a168fce8a947113
mkochan11 <kochanowski.michal.11@gmail.com>
2024-11-12 18:01:00 +0100

    EKF-87 fix: set appropriate signing key

--- src/Identity.API/Program.cs
+++ src/Identity.API/Program.cs
@@ -13,6 +13,7 @@ builder.AddServiceDefaults();
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
