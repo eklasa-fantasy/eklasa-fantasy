e13c0dbcc935e99e5656a6525f04ad4717675aa6
mkochan11 <kochanowski.michal.11@gmail.com>
2024-11-12 16:17:50 +0100

    EKF-98 fix: create and connect identity service to db

--- src/Identity.MigrationService/Program.cs
+++ src/Identity.MigrationService/Program.cs
@@ -1,9 +1,15 @@
+using Identity.API.Data;
 using Identity.MigrationService;
 
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.AddServiceDefaults();
 builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing.AddSource(Worker.ActivitySourceName));
+
+builder.AddSqlServerDbContext<ApplicationDbContext>("sqldata");
 
 var host = builder.Build();
 host.Run();
