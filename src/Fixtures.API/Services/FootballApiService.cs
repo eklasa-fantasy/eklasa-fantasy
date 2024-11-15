21320499b81963b603e8abde06f98323ec4737fe
klos02 <bartoszklosinski02@gmail.com>
2024-11-15 20:10:50 +0100

    EKF-94 feat: add interface and services folders

---
+++ src/Fixtures.API/Services/FootballApiService.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Fixtures.API.Interfaces;
+
+namespace Fixtures.API.Services
+{
+    public class FootballApiService : IFootballApiService
+    {
+        
+    }
+
+    
+}
