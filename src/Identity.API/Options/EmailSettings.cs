9f0cfe860cb1e779b168178e794d45cf273d2cf3
mkochan11 <kochanowski.michal.11@gmail.com>
2024-11-16 16:58:05 +0100

    EKF-103 feat: add email settings class

---
+++ src/Identity.API/Options/EmailSettings.cs
@@ -0,0 +1,9 @@
+namespace Identity.API.Options
+{
+    public class EmailSettings
+    {
+        string ApiKey {get; set;}
+        string SenderEmail {get; set;}
+        string SenderName {get; set;}
+    }
+}
