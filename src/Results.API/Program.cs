82b623dc3839fcfcc5c8f8b6a2da37b615270797
klos02 <bartoszklosinski02@gmail.com>
2024-11-24 17:15:28 +0100

    EKF-107 feat: add football api service to program.cs

--- src/Results.API/Program.cs
+++ src/Results.API/Program.cs
@@ -1,6 +1,11 @@
+using Results.API.Interfaces;
+using Results.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
+DotNetEnv.Env.Load();
 
 // Add services to the container.
 
@@ -8,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IFootballApiService, FootballApiService>(); 
 
 var app = builder.Build();
 
