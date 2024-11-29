1eb12e3f4f79b43c8f82c809c2ca24333271744f
klos02 <bartoszklosinski02@gmail.com>
2024-11-29 17:23:20 +0100

    EKF-126 feat: add fixture dto

---
+++ src/Fixtures.API/Dtos/FixtureDto.cs
@@ -0,0 +1,30 @@
+
+
+namespace Fixtures.API.Dtos
+{
+    
+    public class FixtureDto
+    {
+
+        public string MatchId { get; set; }  
+
+        public string Time { get; set; }
+
+        public string HomeTeamName {get; set; }
+
+        public string AwayTeamName {get; set; }
+
+        public string HomeTeamId {get; set;}
+
+        public string AwayTeamId {get; set;}
+
+        public string Date {get; set;}
+
+        public string Round {get; set;}
+
+        public string HomeTeamBadge {get; set;}
+
+        public string AwayTeamBadge {get; set;}
+
+    }
+}
