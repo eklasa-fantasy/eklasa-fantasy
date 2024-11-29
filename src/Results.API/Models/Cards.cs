63afe4a44af359a14ad686c23d3866eac0d37eea
klos02 <bartoszklosinski02@gmail.com>
2024-11-29 23:44:32 +0100

    EKF-112 feat: add result, goalscorer, cards and subs models

---
+++ src/Results.API/Models/Cards.cs
@@ -0,0 +1,19 @@
+
+
+namespace Results.API.Models
+{
+    public class Cards
+    {
+       
+        public string TimeReceived {get; set;} //bedzie musial zostac string z powodu kartek w doliczonym czasie (90+4)
+
+        
+        public string? HomeFault {get; set;}
+
+        
+        public string? AwayFault {get; set;}
+
+        
+        public string Card {get; set;}
+    }
+}
