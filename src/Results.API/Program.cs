9953cf08024953a9db958e8eb182f74c40466168
domin <domino779@gmail.com>
2024-12-01 17:49:11 +0100

    EKF-116 refactor: change program.cs

--- src/Results.API/Program.cs
+++ src/Results.API/Program.cs
@@ -1,5 +1,7 @@
+using Results.API.Data;
 using Results.API.Interfaces;
 using Results.API.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,14 +10,17 @@ builder.AddServiceDefaults();
 DotNetEnv.Env.Load();
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ResultsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqldata")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IFootballApiService, FootballApiService>(); 
-builder.Services.AddScoped<IResultsService, ResultsService>(); 
+builder.Services.AddScoped<IFootballApiService, FootballApiService>();
+builder.Services.AddScoped<IResultsService, ResultsService>();
 
 var app = builder.Build();
 
